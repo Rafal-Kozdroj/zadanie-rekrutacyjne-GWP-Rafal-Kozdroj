398a9ece80de9277b83eb502adda16624fdb1162
Rafał Kozdrój <rafal.kozdroj+git@gmail.com>
2017-08-21 13:17:57 +0200

    GET sketch

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import http.server
-import socketserver
 import re
+import json
 
 PORT = 8080
+SERVER_ADDRESS = ("127.0.0.1", PORT)
 MAX_VALUE_SIZE = 1024 * 1024 * 1024
 
 values = {}
@@ -14,11 +15,14 @@ class handler(http.server.BaseHTTPRequestHandler):
     def do_PUT(self):
         print("path: " + self.path)
         if not self.path.startswith("/api/objects/"):
-            self.send_error(400)
+            self.send_error(404)
             self.end_headers()
             return
         key = self.path[13:]
         print("key: " + key)
+        if not key_pattern.match(key):
+            self.send_error(400)
+            self.end_headers()
         value = self.rfile.read()
         print("value: " + str(value))
         response = 200
@@ -27,10 +31,21 @@ class handler(http.server.BaseHTTPRequestHandler):
         values[key] = value
         self.send_response(response)
         self.end_headers()
-        return
+
+    def do_GET(self):
+        print("path: " + self.path)
+        if self.path == "/api/objects":
+            # return keys
+            keys = list(values.keys())
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(str(keys).encode())
+            self.wfile.write("\n".encode())
+            return
+        self.send_response(200)
+        self.end_headers()
 
 Handler = handler
 
-with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+httpd = http.server.HTTPServer(SERVER_ADDRESS, Handler)
+httpd.serve_forever()
