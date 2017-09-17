c3c08ee573172f3946a56918621d9d46d21a0d08
Rafał Kozdrój <rafal.kozdroj+git@gmail.com>
2017-09-17 14:25:08 +0200

    Set up test class.

--- tests.py
+++ tests.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python3
 import unittest
+from flask import Flask
 import server
 
-class TestStorage(unittest.TestCase):
+class ServerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        server.app.testing = True
+        self.app = server.app.test_client()
 
     def test_put_key(self):
         values = {}
@@ -10,3 +16,6 @@ class TestStorage(unittest.TestCase):
         server.put_value(key, value, values)
         response = server.get_value(key, values)
         self.assertEqual(response, value)
+
+if __name__ == "__main__":
+    unittest.main()
